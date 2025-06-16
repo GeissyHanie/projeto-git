9a2e9229bf1fd3107da9451f99a9a71d48304839
Geissy Araújo <geissyaraujo@gmail.com>
2025-06-16 13:58:01 -0300

    Corrige script de execução e atualiza teste saucedemo

--- cypress/e2e/saucedemo.spec.js
+++ cypress/e2e/saucedemo.spec.js
@@ -1,6 +1,19 @@
-describe("Sauce demo", () => {
-    beforeEach(() => {
-        cy.visit("https://saucedemo.com");
-    })
-})
+describe('Sauce demo', () => {
+  beforeEach(() => {
+    cy.visit('/'); // Vai usar o baseUrl definido no cypress.env.json
+  });
 
+  it('logs in Successfully', () => {
+    // Preenche o campo usuário
+    cy.get('[data-test="username"]').type('standard_user');
+    
+    // Preenche o campo senha
+    cy.get('[data-test="password"]').type('secret_sauce');
+    
+    // Clica no botão de login
+    cy.get('[data-test="login-button"]').click();
+    
+    // Verifica se redirecionou para a página de inventário
+    cy.url().should('include', '/inventory.html');
+  });
+});
